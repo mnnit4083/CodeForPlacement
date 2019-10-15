805dd926128533ef67d97038a630a7d44271966a
mnnit4083 <42771803+mnnit4083@users.noreply.github.com>
2019-10-15 21:27:58 +0530

    Update KadaneAlgo.cpp

--- KadaneAlgo.cpp
+++ KadaneAlgo.cpp
@@ -7,11 +7,14 @@ int  main(int argc, char const *argv[])
 	int ar[n];
 	for(int i=0;i<n;i++)
 		cin>>ar[i];
+	//initialize maximum as the first element of array
+	// and answer too
 	int maxi=ar[0];
 	int ans=ar[0];
 	for(int i=1;i<n;i++)
-	{
+	{	//either element will contribute to continuios sum or not
 		maxi=max(maxi+ar[i],ar[i]);
+		// pick the maximum till now
 		ans=max(maxi,ans);
 	}
 	cout<<ans<<"\n";
